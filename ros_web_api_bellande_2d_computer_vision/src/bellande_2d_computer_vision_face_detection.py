267b73c5792c1671ee360f608926e260b41c69c7
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-06-29 08:31:01 -0400

    license

--- ros_web_api_bellande_2d_computer_vision/src/bellande_2d_computer_vision_face_detection.py
+++ ros_web_api_bellande_2d_computer_vision/src/bellande_2d_computer_vision_face_detection.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2024 Bellande Robotics Sensors Research Innovation Center, Ronaldson Bellande
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import json
 import os
 import requests
@@ -42,13 +57,12 @@ def image_callback(msg):
 def main():
     global api_url, pub
 
-    # Get the absolute path to the config file
     config_file_path = os.path.join(os.path.dirname(__file__), '../config/configs.json')
-    # Check if the config file exists
+    
     if not os.path.exists(config_file_path):
         print("Config file not found:", config_file_path)
         return
-    # Read configuration from config.json
+    
     with open(config_file_path, 'r') as config_file:
         config = json.load(config_file)
         url = config['url']
